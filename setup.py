d2cfba85d05f3a3bc3004c5e7ad5a5dff3bbb1a9
Tom Keefe <8655118+MisterKeefe@users.noreply.github.com>
2019-09-06 22:14:15 +0100

    bump to 0.1.1 because I left the wrong specifiers in the setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="pytest-involve",
-    version="0.1.0",
+    version="0.1.1",
     author="Tom Keefe",
     author_email="tomlkeefe@gmail.com",
     maintainer="Tom Keefe",
@@ -32,11 +32,7 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Testing",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
