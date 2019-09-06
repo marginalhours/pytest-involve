7b221814795ada2c7207d1d9bbd911b990d77451
Tom Keefe <8655118+MisterKeefe@users.noreply.github.com>
2019-09-06 21:54:38 +0100

    only support >=3.6 for now

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     description="Run tests covering a specific file or changeset",
     long_description=read("README.rst"),
     py_modules=["pytest_involve"],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=3.6",
     install_requires=["pytest>=3.5.0"],
     classifiers=[
         "Development Status :: 4 - Beta",
