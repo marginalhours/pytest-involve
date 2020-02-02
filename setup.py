8da4fd35419a91232798876ec027b73a6113f56e
Tom Keefe <8655118+MisterKeefe@users.noreply.github.com>
2020-02-02 18:20:33 +0000

    experimental py38 support

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
